1344d543e04799b7c5d1632c3408b60db7ef031a
loicmaes <97891718+loicmaes@users.noreply.github.com>
2025-12-21 05:36:25 +0100

    :rocket: add shadcn sonner

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,24 @@
+<script setup lang="ts">
+import "vue-sonner/style.css";
+import { Toaster } from "~/components/ui/sonner";
+import type { Theme } from "~/types/misc/theme";
+
+const theme = computed((): Theme => useColorMode().preference as Theme);
+</script>
+
 <template>
   <div>
     <NuxtRouteAnnouncer />
     <NuxtLayout />
+
+    <ClientOnly>
+      <Toaster
+        :rich-colors="true"
+        close-button
+        position="bottom-right"
+        close-button-position="top-right"
+        :theme="theme"
+      />
+    </ClientOnly>
   </div>
 </template>
