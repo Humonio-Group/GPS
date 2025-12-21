74c1112d7add7b8a5b2b6ffa9f4aac879c963c24
loicmaes <97891718+loicmaes@users.noreply.github.com>
2025-12-21 05:02:18 +0100

    :tada: init learning point project

---
+++ nuxt.config.ts
@@ -0,0 +1,57 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineNuxtConfig({
+  modules: [
+    "@nuxtjs/color-mode",
+    "@nuxtjs/i18n",
+    "shadcn-nuxt",
+    "@nuxt/image",
+    "@nuxt/eslint",
+    "@nuxtjs/google-fonts",
+    "@pinia/nuxt",
+  ],
+  devtools: { enabled: true },
+  css: ["./tailwind.css"],
+  colorMode: {
+    classPrefix: "",
+    classSuffix: "",
+    preference: "system",
+    fallback: "light",
+  },
+  compatibilityDate: "2025-07-15",
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  eslint: {
+    checker: true,
+    config: {
+      stylistic: {
+        indent: 2,
+        semi: true,
+        quotes: "double",
+      },
+    },
+  },
+  googleFonts: {
+    families: {
+      Inter: "100..900",
+    },
+  },
+  i18n: {
+    defaultLocale: "fr",
+    strategy: "no_prefix",
+    locales: [
+      {
+        code: "fr",
+        iso: "fr-FR",
+        name: "Français",
+        file: "fr-FR.json",
+      },
+    ],
+  },
+  shadcn: {
+    prefix: "ui",
+    componentDir: "./app/components/ui",
+  },
+});
