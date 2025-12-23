3780983b0d3e46531ccbfa79a974a800f176efaf
loicmaes <97891718+loicmaes@users.noreply.github.com>
2025-12-23 09:31:59 +0100

    :rocket: add courses page

---
+++ app/pages/[alias]/courses.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { BookDashed } from "lucide-vue-next";
+import PageRoot from "~/components/primitives/composing/PageRoot.vue";
+</script>
+
+<template>
+  <PageRoot class="text-sm text-muted-foreground">
+    <UiEmpty>
+      <UiEmptyHeader>
+        <UiEmptyMedia variant="icon">
+          <BookDashed />
+        </UiEmptyMedia>
+        <UiEmptyTitle>
+          Oups...
+        </UiEmptyTitle>
+        <UiEmptyDescription>
+          Pas encore développé...
+        </UiEmptyDescription>
+      </UiEmptyHeader>
+    </UiEmpty>
+  </PageRoot>
+</template>
