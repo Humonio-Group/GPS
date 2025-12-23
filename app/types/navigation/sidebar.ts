8cda2c2575befdc7a84ae552ae569ec012b8e186
loicmaes <97891718+loicmaes@users.noreply.github.com>
2025-12-23 09:30:58 +0100

    :rocket: add shadcn components

---
+++ app/types/navigation/sidebar.ts
@@ -0,0 +1,33 @@
+import type { LucideIcon } from "lucide-vue-next";
+import type { Version } from "~/types/misc/version";
+
+export interface NavigationGroup {
+  type: "group";
+  label?: string;
+  action?: {
+    icon: LucideIcon;
+    cb: () => void | Promise<void>;
+  };
+  children: NavigationItem[];
+}
+
+export interface NavigationItem {
+  type: "item";
+  label: string;
+  icon: LucideIcon;
+  path: string;
+  exact?: boolean;
+  version?: Version;
+  actions?: Component;
+  children?: NavigationChild[];
+}
+
+export interface NavigationChild {
+  type: "child";
+  label: string;
+  path: string;
+  actions?: Component;
+  version?: Version;
+}
+
+export type NavigationContent = NavigationGroup[];
