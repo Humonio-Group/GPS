2830d1add55333f1462e8c01fa9b412e88ee14f2
loicmaes <97891718+loicmaes@users.noreply.github.com>
2025-12-31 14:49:35 +0100

    :wrench: update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,4 +4,9 @@ import withNuxt from "./.nuxt/eslint.config.mjs";
 export default withNuxt({
   files: ["**/*.{vue,ts}"],
   rules: {},
+}).override("nuxt/typescript/rules", {
+  rules: {
+    "@typescript-eslint/no-explicit-any": "off",
+  },
+  files: ["tests/**/*.ts"],
 });
