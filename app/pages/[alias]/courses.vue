7f4be93e40fada124914df504df679f5a306c5e6
loicmaes <97891718+loicmaes@users.noreply.github.com>
2025-12-31 14:27:09 +0100

    :rocket: setup course parent page

--- app/pages/[alias]/courses.vue
+++ app/pages/[alias]/courses.vue
@@ -1,22 +1,7 @@
 <script setup lang="ts">
-import { BookDashed } from "lucide-vue-next";
-import PageRoot from "~/components/primitives/composing/PageRoot.vue";
+
 </script>
 
 <template>
-  <PageRoot class="text-sm text-muted-foreground">
-    <UiEmpty>
-      <UiEmptyHeader>
-        <UiEmptyMedia variant="icon">
-          <BookDashed />
-        </UiEmptyMedia>
-        <UiEmptyTitle>
-          Oups...
-        </UiEmptyTitle>
-        <UiEmptyDescription>
-          Pas encore développé...
-        </UiEmptyDescription>
-      </UiEmptyHeader>
-    </UiEmpty>
-  </PageRoot>
+  <NuxtPage />
 </template>
