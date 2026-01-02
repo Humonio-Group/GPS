1d60956cfb83969b5f97e5e8921f27c6e4a66074
loicmaes <97891718+loicmaes@users.noreply.github.com>
2026-01-02 19:10:10 +0100

    :rocket: add markdown handler composable and component renderer

---
+++ app/components/primitives/MarkdownRenderer.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { cn } from "~/lib/utils";
+
+interface MarkdownRendererProps {
+  content: string;
+  useMarkdown?: boolean;
+  class?: string;
+}
+
+const props = defineProps<MarkdownRendererProps>();
+const { render } = useMarkdown();
+
+const html = computed(() => render(props.content || ""));
+</script>
+
+<template>
+  <ClientOnly>
+    <div
+      :class="cn('html-wrapper', props.class)"
+      v-html="props.useMarkdown ? html : (content || '')"
+    />
+  </ClientOnly>
+</template>
+
+<style lang="scss">
+.html-wrapper {
+
+}
+</style>
