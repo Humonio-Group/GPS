5e36b772cb91a16c6b6822666254f48f3f2a9e06
loicmaes <97891718+loicmaes@users.noreply.github.com>
2026-01-03 03:13:57 +0100

    :lipstick: add window title when blur and focus back

--- app/app.vue
+++ app/app.vue
@@ -3,14 +3,34 @@ import "vue-sonner/style.css";
 import { Toaster } from "~/components/ui/sonner";
 import type { Theme } from "~/types/misc/theme";
 
+const { t } = useI18n();
+
 useColorMode().preference = "light";
 const theme = computed((): Theme => useColorMode().preference as Theme);
+
+let originalTitle = "";
+
+window.addEventListener("focus", () => {
+  if (!originalTitle.length) return;
+  useHead({
+    title: originalTitle,
+  });
+});
+
+window.addEventListener("blur", () => {
+  originalTitle = document.title;
+
+  useHead({
+    title: t("labels.miss-you"),
+  });
+});
 </script>
 
 <template>
   <div>
     <NuxtRouteAnnouncer />
     <NuxtLayout />
+    <NuxtLoadingIndicator />
 
     <ClientOnly>
       <Toaster
