fb5ddbd6bc7effc6ed40d36f312bf426149b712e
loicmaes <97891718+loicmaes@users.noreply.github.com>
2026-01-03 04:14:02 +0100

    :rocket: add fancy leave title composable

---
+++ app/composables/useFancyLeaveTitle.ts
@@ -0,0 +1,22 @@
+let originalTitle = "";
+
+export const useFancyLeaveTitle = () => {
+  const { t } = useI18n();
+
+  window.addEventListener("focus", () => {
+    if (!originalTitle.length) return;
+
+    useHead({
+      title: originalTitle,
+    });
+
+    originalTitle = "";
+  });
+  window.addEventListener("blur", () => {
+    originalTitle = document.title;
+
+    useHead({
+      title: t("labels.miss-you"),
+    });
+  });
+};
