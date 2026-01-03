4166943f854ffa0b0dc7d0bfe0f68a603a8d5f35
loicmaes <97891718+loicmaes@users.noreply.github.com>
2026-01-03 03:14:28 +0100

    :rocket: add course types

--- app/types/entities/course.ts
+++ app/types/entities/course.ts
@@ -1,5 +1,7 @@
 import type { Nullable } from "~/types/primitives/objects";
 
+export type VideoProvider = "youtube" | "vimeo" | "dailymotion" | "ted";
+
 export interface Program {
   id: number;
   name: string;
@@ -7,6 +9,78 @@ export interface Program {
   picture: string;
   category: Nullable<string>;
 }
+
+export interface Stage {
+  id: number;
+  reference: number;
+  order: number;
+  name: string;
+  description: Nullable<string>;
+  picture: Nullable<string>;
+  locked: boolean;
+  conditions: unknown[];
+  progress: {
+    completed: number;
+    total: number;
+  };
+  contents: Contents;
+}
+export type Stages = Stage[];
+
+export interface Content {
+  id: number;
+  order: number;
+  name: string;
+  description: string;
+  locked: boolean;
+  conditions: unknown[];
+  duration: Nullable<number>;
+  picture: Nullable<string>;
+  dates: {
+    start: Nullable<Date>;
+    end: Nullable<Date>;
+  };
+  permissions: {
+    rateable: boolean;
+    commentable: boolean;
+  };
+  stats: {
+    comments: number;
+    followers: number;
+    likes: number;
+    ratings: number;
+    rate: Nullable<number>;
+    shares: number;
+  };
+  progress: {
+    value: number;
+    viewed: boolean;
+  };
+  activity: {
+    link?: string;
+    image?: string;
+    document?: {
+      name: string;
+      url: string;
+    };
+    video?: {
+      provider: VideoProvider;
+      code: string;
+      url: string;
+    };
+    embed?: {
+      main: boolean;
+      disabled: boolean;
+      label: string;
+      url: string;
+    };
+    results: {
+      label: string;
+      url: string;
+    }[];
+  };
+}
+export type Contents = Content[];
 
 export interface Course {
   id: number;
@@ -20,4 +94,7 @@ export interface Course {
   };
 
   program: Program;
+}
+export interface RichCourse extends Course {
+  stages: Stages;
 }
