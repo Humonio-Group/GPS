dd57d8517e06f53208f40e7d1d063617dbd77c6b
loicmaes <97891718+loicmaes@users.noreply.github.com>
2026-01-05 10:33:52 +0100

    :speech_balloon: migrate translations to details object inside profile page

--- app/pages/[alias]/profile/index.vue
+++ app/pages/[alias]/profile/index.vue
@@ -8,7 +8,7 @@ const { user } = storeToRefs(store);
 const { company } = storeToRefs(useCompanyStore());
 
 useHead({
-  title: `${t("profile.title")} - ${company.value!.name}`,
+  title: `${t("profile.details.title")} - ${company.value!.name}`,
 });
 
 const df = new Intl.DateTimeFormat(locale.value, {
@@ -40,23 +40,23 @@ const df = new Intl.DateTimeFormat(locale.value, {
     <main class="mx-auto w-full max-w-3xl flex flex-col gap-4">
       <ul class="grid *:py-3 divide-y">
         <li class="flex flex-col @md/profile:flex-row @md/profile:items-center @md/profile:justify-between @md/profile:gap-6">
-          <span class="text-muted-foreground text-sm @md/profile:text-base">{{ $t("profile.first-name") }}</span>
+          <span class="text-muted-foreground text-sm @md/profile:text-base">{{ $t("profile.details.first-name") }}</span>
           <p>{{ user!.name.first }}</p>
         </li>
         <li class="flex flex-col @md/profile:flex-row @md/profile:items-center @md/profile:justify-between @md/profile:gap-6">
-          <span class="text-muted-foreground">{{ $t("profile.last-name") }}</span>
+          <span class="text-muted-foreground">{{ $t("profile.details.last-name") }}</span>
           <p>{{ user!.name.last }}</p>
         </li>
         <li class="flex flex-col @md/profile:flex-row @md/profile:items-center @md/profile:justify-between @md/profile:gap-6">
-          <span class="text-muted-foreground text-sm @md/profile:text-base">{{ $t("profile.email") }}</span>
+          <span class="text-muted-foreground text-sm @md/profile:text-base">{{ $t("profile.details.email") }}</span>
           <p>{{ user!.contact.email }}</p>
         </li>
         <li class="flex flex-col @md/profile:flex-row @md/profile:items-center @md/profile:justify-between @md/profile:gap-6">
-          <span class="text-muted-foreground text-sm @md/profile:text-base">{{ $t("profile.phone") }}</span>
+          <span class="text-muted-foreground text-sm @md/profile:text-base">{{ $t("profile.details.phone") }}</span>
           <p>{{ user!.contact.phone }}</p>
         </li>
         <li class="flex flex-col @md/profile:flex-row @md/profile:items-center @md/profile:justify-between @md/profile:gap-6">
-          <span class="text-muted-foreground text-sm @md/profile:text-base">{{ $t("profile.linked-in") }}</span>
+          <span class="text-muted-foreground text-sm @md/profile:text-base">{{ $t("profile.details.linked-in") }}</span>
           <p>
             <NuxtLink
               :to="user!.social.linkedin"
@@ -70,7 +70,7 @@ const df = new Intl.DateTimeFormat(locale.value, {
         </li>
       </ul>
       <p class="text-sm text-muted-foreground self-center">
-        {{ $t("profile.created-at", { date: df.format(user!.dates.creation) }) }}
+        {{ $t("profile.details.created-at", { date: df.format(user!.dates.creation) }) }}
       </p>
     </main>
   </PageRoot>
