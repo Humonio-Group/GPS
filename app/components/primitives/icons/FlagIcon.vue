4643c7d49b9baeaf4e6d496fb0cb4769f1232d79
loicmaes <97891718+loicmaes@users.noreply.github.com>
2026-01-05 13:38:53 +0100

    :rocket: add select from shadcn and flag icon module with custom flag component

---
+++ app/components/primitives/icons/FlagIcon.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { cn } from "~/lib/utils";
+import type { HTMLAttributes } from "vue";
+
+interface FlagIconProps {
+  countryCode: string;
+  class?: HTMLAttributes["class"];
+}
+
+const props = defineProps<FlagIconProps>();
+</script>
+
+<template>
+  <span :class="cn(`fi fi-${countryCode} text-xs`, props.class)" />
+</template>
+
+<style scoped>
+@import "flag-icons/css/flag-icons.min.css";
+</style>
