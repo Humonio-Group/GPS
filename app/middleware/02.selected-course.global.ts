47ad4de55b72a046c617a73a8e92eb70ffa925f4
loicmaes <97891718+loicmaes@users.noreply.github.com>
2026-01-15 08:54:01 +0100

    :truck: migrate logic to course recovery middleware (more controls)

--- app/middleware/02.selected-course.global.ts
+++ app/middleware/02.selected-course.global.ts
@@ -3,9 +3,18 @@ export default defineNuxtRouteMiddleware(async (to) => {
 
   const courseId = Number(to.params.courseId as string);
   const store = useCoursesStore();
-  const { selectedCourse: course } = storeToRefs(store);
+  const { selectedCourse: course, hasStagesLoaded, hasActivitiesLoaded } = storeToRefs(store);
 
   if (course.value?.id === courseId) return;
-  console.log("Loading selected course");
   await store.loadCourse(courseId).then();
+
+  if (!course.value) return navigateTo(`${useWorkspaceUtils().alias.value}/courses`);
+
+  store.loadBadges().then();
+
+  if (hasStagesLoaded.value) {
+    if (!hasActivitiesLoaded.value) store.loadCourseContents().then();
+    return;
+  }
+  store.loadStages().then(() => store.loadCourseContents());
 });
