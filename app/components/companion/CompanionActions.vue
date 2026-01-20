72849665e028b0cd502a71dbd176bf61d17810aa
loicmaes <97891718+loicmaes@users.noreply.github.com>
2026-01-20 17:13:51 +0100

    :rocket: setup companion interface with translations

---
+++ app/components/companion/CompanionActions.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { Archive, Edit2, Trash } from "lucide-vue-next";
+
+interface CompanionActionsProps {
+  conversation: any;
+}
+
+defineProps<CompanionActionsProps>();
+</script>
+
+<template>
+  <UiDropdownMenu>
+    <UiDropdownMenuTrigger as-child>
+      <slot />
+    </UiDropdownMenuTrigger>
+    <UiDropdownMenuContent align="end">
+      <UiDropdownMenuGroup>
+        <UiDropdownMenuItem>
+          <Edit2 />
+          {{ $t("btn.action.rename") }}
+        </UiDropdownMenuItem>
+      </UiDropdownMenuGroup>
+
+      <UiDropdownMenuSeparator />
+
+      <UiDropdownMenuGroup>
+        <UiDropdownMenuItem>
+          <Archive />
+          {{ $t("btn.action.archive") }}
+        </UiDropdownMenuItem>
+        <UiDropdownMenuItem variant="destructive">
+          <Trash />
+          {{ $t("btn.action.delete") }}
+        </UiDropdownMenuItem>
+      </UiDropdownMenuGroup>
+    </UiDropdownMenuContent>
+  </UiDropdownMenu>
+</template>
---
+++ app/pages/[alias]/companion/[conversationSlug].vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+import PageRoot from "~/components/primitives/composing/PageRoot.vue";
+import CompanionMessageBubble from "~/components/companion/CompanionMessageBubble.vue";
+
+definePageMeta({
+  layout: "companion",
+});
+
+const store = useCompanionStore();
+const { selectedConversation: conversation, loading } = storeToRefs(store);
+
+const bottomTrigger = useTemplateRef("bottomTrigger");
+watch(conversation, async () => {
+  await nextTick();
+  bottomTrigger.value?.scrollIntoView({ behavior: "smooth" });
+}, { immediate: true, deep: true });
+
+store.selectConversation(useRoute().params.conversationSlug as string);
+
+onMounted(() => bottomTrigger.value?.scrollIntoView({ behavior: "smooth" }));
+onBeforeRouteLeave(() => store.selectConversation());
+</script>
+
+<template>
+  <PageRoot
+    name="companion.conversation"
+    class="flex-1 flex mx-auto max-w-5xl w-full flex-col gap-6 pb-48 pt-16"
+  >
+    <div
+      v-if="loading.item && !conversation.messages.length"
+      class="h-24 grid place-items-center w-full"
+    >
+      <UiSpinner />
+    </div>
+
+    <template v-if="conversation?.messages.length">
+      <CompanionMessageBubble
+        v-for="(message, index) in conversation.messages"
+        :key="index"
+        :message="message.message"
+        :markdown="message.author === 'agent'"
+        :reverse="message.author === 'user'"
+      />
+    </template>
+
+    <span
+      v-if="loading.thinking"
+      class="animate-pulse size-2.5 rounded-full bg-foreground"
+    />
+
+    <div
+      ref="bottomTrigger"
+      class="h-0"
+    />
+  </PageRoot>
+</template>
