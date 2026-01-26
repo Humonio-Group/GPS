60618c55c52fe4c208ae50bb4626b3cccb42c4d3
loicmaes <97891718+loicmaes@users.noreply.github.com>
2026-01-26 11:31:44 +0100

    :rocket: add program-go button binding

--- app/stores/courses.ts
+++ app/stores/courses.ts
@@ -262,7 +262,7 @@ function buildContentEntity(data: any, included: any): Content {
 
   // Interactive contents
   // Forms
-  if (data.attributes.specific.subtype === 2 || data.attributes.specific.type === 4) {
+  if (data.attributes.specific.subtype === 2 || data.attributes.specific.type === 4 || (data.attributes.specific.type === 9 && data.attributes.specific.subtype === 4)) {
     const form = buildFormActivity(data);
     if (form) activity = { ...activity, embed: form };
   }
