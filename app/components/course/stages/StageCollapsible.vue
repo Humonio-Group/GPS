e29c03c90a21303d7fff3740f6dd970676bb1503
loicmaes <production@maesloic.fr>
2026-02-02 12:54:02 +0100

    :ambulance: fix content regression when fetching

--- app/components/course/stages/StageCollapsible.vue
+++ app/components/course/stages/StageCollapsible.vue
@@ -14,7 +14,9 @@ const { loading: loads } = storeToRefs(store);
 
 const loading = computed(() => loads.value.specific.stageContents.includes(props.stage.reference));
 
-onMounted(() => store.loadContents(props.stage.reference));
+onMounted(() => {
+  if (!props.stage.contents.length) store.loadContents(props.stage.reference);
+});
 </script>
 
 <template>
