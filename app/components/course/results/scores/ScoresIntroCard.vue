1e0e4c769bf698f9765bea72768be0b4c2b7735b
loicmaes <production@maesloic.fr>
2026-02-11 13:04:36 +0100

    :rocket: add scores section

---
+++ app/components/course/results/scores/ScoresIntroCard.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { Gauge } from "lucide-vue-next";
+</script>
+
+<template>
+  <UiCard>
+    <UiCardHeader>
+      <UiCardTitle class="inline-flex items-center gap-2">
+        <Gauge class="size-4" />
+        {{ $t("courses.specimen.results.scores.title") }}
+      </UiCardTitle>
+
+      <UiCardDescription>
+        {{ $t("courses.specimen.results.scores.description") }}
+      </UiCardDescription>
+    </UiCardHeader>
+
+    <UiCardContent />
+  </UiCard>
+</template>
