226323dcb797549da1c7404aaa35fbd010efee0f
loicmaes <production@maesloic.fr>
2026-02-19 15:50:25 +0100

    :rocket: add smart redirections and home page handling

--- app/middleware/01.company.global.ts
+++ app/middleware/01.company.global.ts
@@ -1,7 +1,14 @@
+const publicPaths = [
+  "/auth/login",
+  "/auth/portal",
+];
+
 export default defineNuxtRouteMiddleware(async (to) => {
   const alias = to.params.alias;
+  const { user } = storeToRefs(useUserStore());
   const store = useCompanyStore();
 
+  if (user.value && !alias && !publicPaths.includes(to.path)) return navigateTo("/auth/portal");
   if (store.isLoaded && storeToRefs(store).company.value!.alias === alias) return;
 
   await store.fetchCompany(alias as string);
---
+++ app/pages/[alias]/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import PageRoot from "~/components/primitives/composing/PageRoot.vue";
+
+onBeforeMount(() => {
+  const { alias } = useWorkspaceUtils();
+  navigateTo(`/${alias.value}/courses`);
+});
+</script>
+
+<template>
+  <PageRoot name="home">
+    <!-- todo: implement home page (as dashboard) - loic -->
+  </PageRoot>
+</template>
