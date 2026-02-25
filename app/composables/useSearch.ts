fe0e9d48a80b7bc8b4fcbed090d7d6d27ffdcd94
loicmaes <production@maesloic.fr>
2026-02-25 09:03:40 +0100

    :rocket: add create action from template modal

--- app/composables/useSearch.ts
+++ app/composables/useSearch.ts
@@ -2,7 +2,7 @@ export function useSearch<T = any>(serie: Ref<T[]>, ...searchKeys: string[]) {
   const search = ref<string>("");
 
   const clear = () => search.value = "";
-  const sanitize = (input: string) => input.trim().toLowerCase();
+  const sanitize = (input: string) => input?.trim().toLowerCase();
   const matches = (...values: string[]): boolean => values.some(v => sanitize(v).includes(sanitize(search.value || "")));
 
   const resolve = (obj: unknown, path: string): string =>
